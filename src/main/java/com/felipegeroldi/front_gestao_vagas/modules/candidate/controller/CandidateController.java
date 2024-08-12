c7960275c45535dbf875881914f816725e31a184
Felipe Geroldi <142124821+fgeroldi@users.noreply.github.com>
2024-08-12 16:11:37 -0300

    adjust access token

--- src/main/java/com/felipegeroldi/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/felipegeroldi/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,16 +1,12 @@
 package com.felipegeroldi.front_gestao_vagas.modules.candidate.controller;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.HttpClientErrorException;
@@ -55,8 +51,8 @@ public class CandidateController {
 
             var auth = new UsernamePasswordAuthenticationToken(null, null, grants);
             auth.setDetails(token.getAccessToken());
-
             SecurityContextHolder.getContext().setAuthentication(auth);
+
             session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
             session.setAttribute("token", token);
 
--- src/main/java/com/felipegeroldi/front_gestao_vagas/modules/candidate/dto/TokenDTO.java
+++ src/main/java/com/felipegeroldi/front_gestao_vagas/modules/candidate/dto/TokenDTO.java
@@ -2,10 +2,13 @@ package com.felipegeroldi.front_gestao_vagas.modules.candidate.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class TokenDTO {
+    @JsonProperty("access_token")
     private String accessToken;
     private List<String> roles;
 }
